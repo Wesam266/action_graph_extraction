008369f53a088a419f8a106daed6d12f4794b1d6
skckompella <skckompella@gmail.com>
2017-05-04 13:47:09 -0400

    Updating

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,9 @@ OP_SIG_MODEL_FILE='opSigModel.pkl'
 RAW_MATERIAL_MODEL_FILE='rawMaterialModel.pkl'
 PART_COMP_MODEL_FILE='partCompositeModel.pkl'
 APP_MODEL_FILE='apparatusModel.pkl'
+
+
+MATERIAL_TAG = 'material'
+APPARATUS_TAG = 'apparatus'
+INTERMEDIATE_PRODUCT_TAG = 'intrmed'
+IMPLICIT_ARGUMENT_TAG = 'IMPLICIT_ARG'
