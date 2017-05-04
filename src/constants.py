886bc04b0831cf572b7c611c914935db8f3ac688
skckompella <skckompella@gmail.com>
2017-05-04 09:37:28 -0400

    Adding constants.py

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+
+ACTION_GRAPH_FILE='actionGraphs.pkl'
+OP_SIG_MODEL_FILE='opSigModel.pkl'
+RAW_MATERIAL_MODEL_FILE='rawMaterialModel.pkl'
+PART_COMP_MODEL_FILE='partCompositeModel.pkl'
+APP_MODEL_FILE='apparatusModel.pkl'
